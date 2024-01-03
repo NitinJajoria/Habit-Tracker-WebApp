0ad7184a74b5c8f4d52afbb8133bd018ee6486df
NitinJajoria <95345434+NitinJajoria@users.noreply.github.com>
2024-01-03 07:30:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,39 @@
-// require express
 const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 9000;
-
+const configureSession = require("./config/sessionConfig");
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
 const db = require("./config/mongoose");
-
-// require connect-flash
 const flash = require("connect-flash");
 const flashMiddleware = require("./config/flashMiddleware");
-
-// used for session cookies
-const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport_local");
 
-const MongoStore = require("connect-mongo");
-
-// layouts for ejs
+// Middleware configuration
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// set up the view engine
+// Setting view engine as "EJS"
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("./assets"));
 
-//mongo store is used to store the session cookie
-app.use(
-	session({
-		name: "habitTracker",
-		secret: "12345",
-		saveUninitialized: false,
-		resave: false,
-		cookie: {
-			maxAge: 1000 * 60 * 100,
-		},
-		store: MongoStore.create(
-			{
-				mongoUrl:
-					"mongodb+srv://nitinjajoria97:nitinjajoria123@cluster1.798a1pd.mongodb.net/HabitTracker?retryWrites=true&w=majority",
-				autoRemover: "disabled",
-			},
-			function (err) {
-				console.log("Error in the mongo-store");
-			}
-		),
-	})
-);
-
-// Using passport
+// Passport configuration
+app.use(configureSession()); // express-session
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
-// flash middleware
+// flash connection
 app.use(flash());
 app.use(flashMiddleware.setFlash);
 
-// use express router
+// Route middleware
 app.use("/", require("./routes"));
 
-// directing the app in the given port
-app.listen(port, function (err) {
+app.listen(port, (err) => {
 	if (err) {
 		console.log("Error", err);
 		return;
